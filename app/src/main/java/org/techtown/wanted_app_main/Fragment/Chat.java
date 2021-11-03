50fd9115ca829b671e5b13407b0a0f29ff9e6320
seonju0147 <o6o4_@naver.com>
2021-11-03 16:07:32 +0900

    [feat] 채팅탭 xml 완성

---
+++ app/src/main/java/org/techtown/wanted_app_main/Fragment/Chat.java
@@ -0,0 +1,37 @@
+package org.techtown.wanted_app_main.Fragment;
+
+public class Chat {
+    public String nickname;
+    public String content;
+    public int imgRes;
+
+    public Chat(String nickname, String content, int imgRes) {
+        this.nickname = nickname;
+        this.content = content;
+        this.imgRes = imgRes;
+    }
+
+    public String getNickame() {
+        return nickname;
+    }
+
+    public void setNickame(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getImgRes() {
+        return imgRes;
+    }
+
+    public void setImgRes(int imgUrl) {
+        this.imgRes = imgUrl;
+    }
+}

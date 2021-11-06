39e05eebf185959aaabc54a8febaaf997155b640
정석우 <ggp03016@naver.com>
2021-11-07 03:17:26 +0900

    [feat] 데이터를 받는 다오객체 생성 1. PersonalDtoInPosting - 포스팅 안의 사람 정보 받는 DTO 2. PostingDtoInPersonal - 사람안의 포스팅 정보를 받는 DTO
    [feat] 리퀘스트를 보내는 파일 생성
    1. GetPersonalsRequest - 모든 사람 정보 가져옴
    
    [fix] 데이터 받는 형태 수정
    1. Personal
    2. Posting
    3. Connect
    
    [fix] 프래그먼트 수정
    1. MainFragment
    2. BoardFragment

---
+++ app/src/main/java/org/techtown/wanted_app_main/database/Dto/PersonalDtoInPosting.java
@@ -0,0 +1,33 @@
+package org.techtown.wanted_app_main.database.Dto;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+public class PersonalDtoInPosting {
+
+    @SerializedName("id")
+    @Expose
+    public Long id;
+    public String nickname;
+    public String img;
+//    @SerializedName("postings")
+//    @Expose
+
+
+    public PersonalDtoInPosting(Long id, String nickname, String img) {
+        this.id = id;
+        this.nickname = nickname;
+        this.img = img;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonalDtoInPosting{" +
+                "id=" + id +
+                ", nickname='" + nickname + '\'' +
+                ", img='" + img + '\'' +
+                '}';
+    }
+}
--- app/src/main/java/org/techtown/wanted_app_main/database/Personal.java
+++ app/src/main/java/org/techtown/wanted_app_main/database/Personal.java
@@ -1,9 +1,15 @@
 package org.techtown.wanted_app_main.database;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import org.techtown.wanted_app_main.database.Dto.PostingDtoInPersonal;
+
 import java.util.ArrayList;
-import java.util.List;
 
 public class Personal {
+    @SerializedName("id")
+    @Expose
     public Long id;
     public String stringId;
     public String pwd;
@@ -16,9 +22,12 @@ public class Personal {
     public String career;
     public Integer gender;
     public String img;
+    @SerializedName("postings")
+    @Expose
+    public ArrayList<PostingDtoInPersonal> postings;
 
     public Personal(Long id, String stringId, String pwd, String nickname, String school, String major, Integer grade, Integer age,
-                    String address, String career, Integer gender, String img) {
+                    String address, String career, Integer gender, String img, ArrayList postings) {
         this.id = id;
         this.stringId = stringId;
         this.pwd = pwd;
@@ -31,6 +40,29 @@ public class Personal {
         this.career = career;
         this.gender = gender;
         this.img = img;
+        if (postings != null) {
+            this.postings = postings;
+        } else{
+            postings = new ArrayList<>();
+        }
     }
 
+    @Override
+    public String toString() {
+        return "Personal{" +
+                "id=" + id +
+                ", stringId='" + stringId + '\'' +
+                ", pwd='" + pwd + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", school='" + school + '\'' +
+                ", major='" + major + '\'' +
+                ", grade=" + grade +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                ", career='" + career + '\'' +
+                ", gender=" + gender +
+                ", img='" + img + '\'' +
+                ", postings='" + postings +
+                '}';
+    }
 }

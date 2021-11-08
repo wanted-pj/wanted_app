448045e0275e6db1cd4503aa40c5777291264ae2
sia7997 <sia7997@naver.com>
2021-11-08 21:21:48 +0900

    [fix] Login editext수정

--- app/src/main/java/org/techtown/wanted_app_main/Activity/Login/LoginActivity.java
+++ app/src/main/java/org/techtown/wanted_app_main/Activity/Login/LoginActivity.java
@@ -7,6 +7,7 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -42,7 +43,7 @@ public class LoginActivity extends AppCompatActivity {
     private ArrayList<Personal> list = new ArrayList<>();
     private EditText getId;
     private EditText getPwd;
-    private TextView registerbtn;
+    ViewGroup registerbtn;
     ViewGroup loginbtn;
     private boolean check = false;
     Dialog dialog;

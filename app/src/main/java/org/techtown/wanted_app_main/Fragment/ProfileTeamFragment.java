d68c6aa2bb28273bf2000982702e0cb2f078c465
seonju0147 <o6o4_@naver.com>
2021-11-12 15:22:07 +0900

    [feat] ProfileTeamStarFragment 생성

--- app/src/main/java/org/techtown/wanted_app_main/Fragment/ProfileTeamFragment.java
+++ app/src/main/java/org/techtown/wanted_app_main/Fragment/ProfileTeamFragment.java
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -58,6 +59,17 @@ public class ProfileTeamFragment extends Fragment {
         members.add(new Friend("다니엘", "경기대학교", "경영학과", "청주시", getResources().getIdentifier("@drawable/profile_basic5", "drawable", getContext().getPackageName())));
         members.add(new Friend("스콧", "수원대학교", "컴퓨터공학과", "시흥시", getResources().getIdentifier("@drawable/profile_basic6", "drawable", getContext().getPackageName())));
         memberAdapter.setFriendList(members);
+
+        // 별점 매기는 페이지로 이동
+        TextView goStar = view.findViewById(R.id.profile_team_go_star);
+        goStar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ProfileTeamStarFragment dialog = new ProfileTeamStarFragment();
+                dialog.show(getActivity().getSupportFragmentManager(),"star_dialog");
+            }
+        });
 
         return view;
     }
